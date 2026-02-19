316ce29c8a134000eae34171a50138ff7fb4794e
ykttdn <38461803+ykttdn@users.noreply.github.com>
2026-02-20 00:31:25 +0900

    Exclude gitignored files from ESLint's target

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,8 +3,13 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default defineConfig([
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
     plugins: { js },
