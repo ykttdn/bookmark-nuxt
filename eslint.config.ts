8e64365970d81b075edfad8e105779cf7b2f0bc7
ykttdn <38461803+ykttdn@users.noreply.github.com>
2026-02-21 23:10:34 +0900

    Customize eslint-plugin-vue rules for Nuxt

--- eslint.config.ts
+++ eslint.config.ts
@@ -17,6 +17,13 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  pluginVue.configs["flat/recommended-error"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    rules: {
+      "no-undef": "off",
+      "vue/multi-word-component-names": "off",
+    },
+  },
 ]);
